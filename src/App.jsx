a9019bf03d2768bb40781b0029d3547b0b93d555
Caio Balieiro <129430412+CaioBalieir0@users.noreply.github.com>
2024-12-18 08:14:40 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div
       className="d-flex justify-content-center align-items-center"
-      style={{ width: "100%", height: "100vh" }} // Define a altura total da tela
+      style={{ width: "100%", height: "100vh" }}
     >
       <CustomCard />
     </div>
